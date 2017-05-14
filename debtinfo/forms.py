dafcd9b650c366f0487d141fd79b09fbc3e25198
Paul Aswa <aswapaul@gmail.com>
2017-05-14 13:41:23 +0300

    Add static files

--- debtinfo/forms.py
+++ debtinfo/forms.py
@@ -5,8 +5,8 @@ from django import forms
 class SearchForm(forms.Form):
     """Configure and return a search form."""
 
-    q = forms.CharField(required=True)
+    q = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'validate'}))
 
     def __init__(self, *args, **kwargs):
         super(SearchForm, self).__init__(*args, **kwargs)
-        self.fields['q'].label = ''
+        self.fields['q'].label = 'Search'
